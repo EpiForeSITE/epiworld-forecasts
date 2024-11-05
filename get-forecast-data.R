4c1058730b9cdf0aa7f714ea6a2e33ef73f576b1
Andrew Pulsipher <a.pulsipher@utah.edu>
2024-11-05 15:03:25 -0700

    Clean up get-forecast-data.R to improve readability

--- get-forecast-data.R
+++ get-forecast-data.R
@@ -1,18 +1,17 @@
 # Download the zipped data from Utah DHHS
-temp <- tempfile()
-download.file(
-  "https://coronavirus-dashboard.utah.gov/Utah_COVID19_data.zip",
-  temp
-)
+data_url <- "https://coronavirus-dashboard.utah.gov/Utah_COVID19_data.zip"
+temp_file <- tempfile()
+download.file(data_url, temp_file)
 # Extract the appropriate data file
+target_file_regex <- "Trends_Epidemic+"
 datafile_name <- grep(
-  "Trends_Epidemic+",
-  unzip(temp, list = TRUE)$Name,
+  target_file_regex,
+  unzip(temp_file, list = TRUE)$Name,
   ignore.case = TRUE,
   value = TRUE
 )
-datafile <- unz(temp, datafile_name)
+datafile <- unz(temp_file, datafile_name)
 # Extract the data
 forecast_data <- read.csv(datafile)
 # Cleanup
-unlink(temp)
+unlink(temp_file)
