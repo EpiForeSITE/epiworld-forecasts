225293fda2752724d6b48dd580cf4bb93e0f3d47
Andrew Pulsipher <45372570+apulsipher@users.noreply.github.com>
2024-11-06 06:42:05 -0700

    Clean up data scraping code (#30)
    * Clean up get-forecast-data.R to improve readability
    
    * Add error handling to data download

--- get-forecast-data.R
+++ get-forecast-data.R
@@ -1,18 +1,33 @@
-# Download the zipped data from Utah DHHS
-temp <- tempfile()
-download.file(
-  "https://coronavirus-dashboard.utah.gov/Utah_COVID19_data.zip",
-  temp
-)
-# Extract the appropriate data file
-datafile_name <- grep(
-  "Trends_Epidemic+",
-  unzip(temp, list = TRUE)$Name,
-  ignore.case = TRUE,
-  value = TRUE
-)
-datafile <- unz(temp, datafile_name)
-# Extract the data
-forecast_data <- read.csv(datafile)
-# Cleanup
-unlink(temp)
+# Downloads data from the given 'url' and
+# extracts the data file that matches the 'target_file'
+get_forecast_data <- function(data_url, target_file) {
+  tryCatch({
+    # Download the zipped data
+    temp_file <- tempfile()
+    download.file(data_url, temp_file)
+    # Select the appropriate data file
+    datafile_name <- grep(
+      target_file,
+      unzip(temp_file, list = TRUE)$Name,
+      ignore.case = TRUE,
+      value = TRUE
+    )
+    datafile <- unz(temp_file, datafile_name)
+    # Extract the data
+    forecast_data <- read.csv(datafile)
+    return(forecast_data)
+  },
+  error = function(cond) {
+    message("Error: ", conditionMessage(cond))
+    return(-1)
+  },
+  warning = function(cond) {
+    message("Warning: ", conditionMessage(cond))
+    return(-2)
+  },
+  finally = {
+    # Cleanup
+    unlink(temp_file)
+  }
+  )
+}
