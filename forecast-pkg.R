936fab3010104872a86034675ce6e4f7b7b5438a
Andrew Pulsipher <a.pulsipher@utah.edu>
2025-01-14 16:08:36 -0700

    Refactor get_season_starts

--- forecast-pkg.R
+++ forecast-pkg.R
@@ -56,10 +56,10 @@ get_season_starts <- function(dates) {
   seasons <- mapply(get_date_season, dates)
   season_names <- c("spring", "summer", "fall", "winter")
 
-  spring_start <- match(season_names[1], forecast_seasons, nomatch = -1)
-  summer_start <- match(season_names[2], forecast_seasons, nomatch = -1)
-  fall_start <- match(season_names[3], forecast_seasons, nomatch = -1)
-  winter_start <- match(season_names[4], forecast_seasons, nomatch = -1)
+  spring_start <- match(season_names[1], seasons, nomatch = -1)
+  summer_start <- match(season_names[2], seasons, nomatch = -1)
+  fall_start <- match(season_names[3], seasons, nomatch = -1)
+  winter_start <- match(season_names[4], seasons, nomatch = -1)
 
   season_starts <- c(spring_start, summer_start, fall_start, winter_start)
   names(season_starts) <- season_names
