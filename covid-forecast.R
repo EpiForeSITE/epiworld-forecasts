981c9cef5191606ba363dc3dc58eec8c8e960b51
Andrew Pulsipher <a.pulsipher@utah.edu>
2025-01-15 15:19:35 -0700

    Finish refactor, compartmentalizing code into separate files

--- covid-forecast.R
+++ covid-forecast.R
@@ -89,5 +89,191 @@ get_params_sample <- function(lfmcmc_obj, total_samples, burnin, sample_size) {
 }
 
 ## ----------------------------------
+## Setup Forecast
+## ----------------------------------
+# Define forecast parameters
+n_days <- 90 # Calibrate model last 90 days of data
+
+## ----------------------------------
+## Get Data
+## ----------------------------------
+
+# Get COVID-19 data
+covid_data <- get_covid_data(n_days)
+# Compute start date for each season
+seasons <- get_season_starts(covid_data$Date)
+# Get observed case counts
+covid_cases <- covid_data$Daily.Cases
+
+## ----------------------------------
+## Create SIR CONN Model
+## ----------------------------------
+
+# Define SIRCONN model parameters
+model_seed          <- 112      # Random seed
+model_ndays         <- n_days   # How many days to run the model
+model_n             <- 10000    # model population size
+
+# Define initial disease parameters
+init_prevalence <- covid_cases[1] / model_n
+init_contact_rate <- 10
+init_transmission_rate <- 0.05
+init_recovery_rate <- 1 / 7
+
+# Create the SIRCONN model
+covid_sirconn_model <- ModelSIRCONN(
+  name              = "COVID-19",
+  n                 = model_n,
+  prevalence        = init_prevalence,
+  contact_rate      = init_contact_rate,
+  transmission_rate = init_transmission_rate,
+  recovery_rate     = init_recovery_rate
+)
+
+## ----------------------------------
+## Setup LFMCMC Calibration
+## ----------------------------------
+
+# Define LFMCMC parameters
+lfmcmc_n_samples <- 6000   # number of LFMCMC iterations
+lfmcmc_burnin <- 2000    # burn-in period
+lfmcmc_epsilon <- 0.25
+
+init_lfmcmc_params <- c(
+  1 / 7,  # r_rate
+  0.05,   # t_rate_spring
+  0.04,   # t_rate_summer
+  0.06,   # t_rate_fall
+  0.07,   # t_rate_winter
+  10,     # c_rate_weekday
+  2       # c_rate_weekend
+)
+param_names <- c(
+  "Recovery rate",
+  "Transmission rate (spring)",
+  "Transmission rate (summer)",
+  "Transmission rate (fall)",
+  "Transmission rate (winter)",
+  "Contact rate (weekday)",
+  "Contact rate (weekend)"
+)
+
+stats_names <- c(
+  "Time to peak",
+  "Size of peak",
+  "Mean (cases)",
+  "Standard deviation (cases)"
+)
+
+# Define the LFMCMC simulation function
+lfmcmc_simulation_fun <- function(params) {
+  # Extract parameters
+  r_rate          <- params[1]
+  t_rate_spring   <- params[2]
+  t_rate_summer   <- params[3]
+  t_rate_fall     <- params[4]
+  t_rate_winter   <- params[5]
+  c_rate_weekday  <- params[6]
+  c_rate_weekend  <- params[7]
+
+  # Set recovery rate
+  set_param(covid_sirconn_model, "Recovery rate", r_rate)
+
+  # Global event to change contact and transmission rates
+  change_c_and_t_rates <- function(model) {
+    # Get the current model day (step)
+    current_model_day <- today(model)
+
+    ## Update contact rate based on weekday/weekend
+    if (any(c(6, 0) %in% (current_model_day %% 7L))) {
+      set_param(model, "Contact rate", c_rate_weekend)
+    } else {
+      set_param(model, "Contact rate", c_rate_weekday)
+    }
+
+    ## Update transmission rate each season
+    if (current_model_day == seasons[["spring"]]) {
+      set_param(model, "Transmission rate", t_rate_spring)
+    } else if (current_model_day == seasons[["summer"]]) {
+      set_param(model, "Transmission rate", t_rate_summer)
+    } else if (current_model_day == seasons[["fall"]]) {
+      set_param(model, "Transmission rate", t_rate_fall)
+    } else if (current_model_day == seasons[["winter"]]) {
+      set_param(model, "Transmission rate", t_rate_winter)
+    }
+
+    invisible(model)
+  }
+
+  # Add global event to the model
+  change_c_and_t_event_name <- "Change Contact and Transmission Rates"
+  globalevent_fun(change_c_and_t_rates, name = change_c_and_t_event_name) |>
+    add_globalevent(model = covid_sirconn_model)
+
+  # Run the model
+  verbose_off(covid_sirconn_model)
+  run(covid_sirconn_model, ndays = model_ndays)
+
+  # Remove global event (new event set each simulation run)
+  rm_globalevent(covid_sirconn_model, change_c_and_t_event_name)
+
+  # Get infected cases
+  hist_matrix <- get_hist_transition_matrix(covid_sirconn_model)
+  # - Drop the last day of data because the model returns data for (model_ndays + 1) days
+  infected_cases <- head(
+    hist_matrix[
+      hist_matrix$state_to == "Infected" &
+        hist_matrix$state_from == "Susceptible",
+      c("counts")],
+    -1
+  )
+
+  return(as.double(infected_cases))
+}
+
+# Create the LFMCMC object
+calibration_lfmcmc <- LFMCMC(covid_sirconn_model) |>
+  set_simulation_fun(lfmcmc_simulation_fun) |>
+  set_summary_fun(lfmcmc_summary_fun) |>
+  set_proposal_fun(lfmcmc_proposal_fun) |>
+  set_kernel_fun(lfmcmc_kernel_fun) |>
+  set_observed_data(covid_cases)
+
+# Run LFMCMC calibration
+run_lfmcmc(
+  lfmcmc = calibration_lfmcmc,
+  params_init_ = init_lfmcmc_params,
+  n_samples_ = lfmcmc_n_samples,
+  epsilon_ = lfmcmc_epsilon,
+  seed = model_seed
+)
+set_params_names(calibration_lfmcmc, param_names)
+set_stats_names(calibration_lfmcmc, stats_names)
+
+## ----------------------------------
 ## RUN FORECAST
 ## ----------------------------------
+# Create a new SIR CONN model
+# - Compute prevalance based on most recent day
+forecast_prevalence <- covid_cases[90] / model_n
+# - Init the model
+covid_sirconn_model <- ModelSIRCONN(
+  name              = "COVID-19",
+  n                 = model_n,
+  prevalence        = forecast_prevalence,
+  contact_rate      = init_contact_rate,
+  transmission_rate = init_transmission_rate,
+  recovery_rate     = init_recovery_rate
+)
+
+# Run the simulation for each set of params in the sample
+# - Select sample of accepted params from LFMCMC
+forecast_sample_n <- 200 # Sample size
+params_sample <- get_params_sample(calibration_lfmcmc,
+  lfmcmc_n_samples,
+  lfmcmc_burnin,
+  forecast_sample_n)
+# - Set forecast length
+model_ndays <- 14
+# - Run simulation function for each set of params from the sample
+forecast_dist <- apply(params_sample, 1, lfmcmc_simulation_fun)
